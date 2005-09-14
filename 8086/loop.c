07c309a819381aa2feb1dcbbc497772b5e1b3232
Andreas Kling <kling@webkit.org>
2005-09-14 19:31:31 +0000

    Detect REP STOS and REP MOVS and perform them "natively" instead of calling the opcode handlers over and over.
    Only handles DF=0 (incremental) situations so far.
    
    Enabled at compile-time by #define VOMIT_NATIVE_REP_STUFF

--- 8086/loop.c
+++ 8086/loop.c
@@ -6,6 +6,10 @@
 
 #include "vomit.h"
 
+#ifdef VOMIT_NATIVE_REP_STUFF
+#include <string.h>
+#endif
+
 void
 _LOOP_imm8()
 {
@@ -41,6 +45,10 @@ _REP()
 {
 	byte rop = cpu_pfq_getbyte();
 	word *old_segment = CurrentSegment;
+#ifdef VOMIT_NATIVE_REP_STUFF
+	byte *bdest, *bsrc;
+	word *wdest;
+#endif
 
 	/* If the following opcode is a segment override instruction,
 	 * we override our "CurrentSegment" accordingly and use the
@@ -55,6 +63,64 @@ _REP()
 		}
 		rop = cpu_pfq_getbyte();
 	}
+
+#ifdef VOMIT_NATIVE_REP_STUFF
+
+	/* Detect incremental REP STOS and REP MOVS and perform them
+	 * natively (in C, at least) instead. */
+
+	if ( DF == 0 ) {
+
+		/* REP STOSB */
+		if ( rop == 0xAA ) {
+			bdest = mem_space + ( ES << 4 ) + DI;
+			memset( bdest, *treg8[REG_AL], CX );
+			DI += CX;
+			CX = 0;
+			return;
+		}
+
+		/* REP STOSW */
+		else if ( rop == 0xAB ) {
+			if ( *treg8[REG_AH] != *treg8[REG_AL] ) {
+				wdest = (word *)(mem_space + ( ES << 4 ) + DI);
+				DI += CX * 2;
+				for( ; CX; --CX ) {
+					*(wdest++) = AX;
+				}
+			} else {
+				bdest = mem_space + ( ES << 4 ) + DI;
+				memset( bdest, *treg8[REG_AL], CX * 2 );
+				DI += CX * 2;
+			}
+			CX = 0;
+			return;
+		}
+
+		/* REP MOVSB */
+		else if ( rop == 0xA4 ) {
+			bdest = mem_space + ( ES << 4 ) + DI;
+			bsrc = mem_space + ( *CurrentSegment << 4 ) + SI;
+			DI += CX;
+			SI += CX;
+			memcpy( bdest, bsrc, CX );
+			CX = 0;
+			return;
+		}
+
+		/* REP MOVSW */
+		else if ( rop == 0xA5 ) {
+			bdest = mem_space + ( ES << 4 ) + DI;
+			bsrc = mem_space + ( *CurrentSegment << 4 ) + SI;
+			DI += CX * 2;
+			SI += CX * 2;
+			memcpy( bdest, bsrc, CX * 2 );
+			CX = 0;
+			return;
+		}
+	}
+
+#endif
 
 	/* If not CMPS or SCAS -- REP, else REPE */
 	if ( rop != 0xA6 && rop != 0xA7 && rop != 0xAE && rop != 0xAF ) {
