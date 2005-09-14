f55317fcfc3ac9fb383661f4f84c5adaef58a961
Andreas Kling <kling@webkit.org>
2005-09-14 16:26:28 +0000

    Oops, cpu_evaluate() shouldn't have to mask the condition.

--- 8086/cpu.c
+++ 8086/cpu.c
@@ -419,7 +419,7 @@ void cpu_addinstruction(int lowop, int highop, void (*function)(), char *mnemoni
 bool
 cpu_evaluate( byte condition )
 {
-	switch ( condition & 0x0F ) {
+	switch ( condition ) {
 		case  0: return OF;                       /* O          */
 		case  1: return !OF;                      /* NO         */
 		case  2: return CF;                       /* B, C, NAE  */
