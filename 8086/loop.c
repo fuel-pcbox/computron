ca3cf1e9270cdbcf35fca47ffa674b17831c0055
Andreas Kling <kling@webkit.org>
2005-09-14 15:56:39 +0000

    Corrected REP instructions' handling of segment overrides + cleanups.

--- 8086/loop.c
+++ 8086/loop.c
@@ -11,8 +11,9 @@ _LOOP_imm8()
 {
 	byte disp = cpu_pfq_getbyte();
 	CX--;
-	if (CX != 0)
-		cpu_jump(CS, IP+(sigbyte)disp);
+	if ( CX ) {
+		cpu_jump( CS, IP + (sigbyte)disp );
+	}
 }
 
 void
@@ -20,9 +21,9 @@ _LOOPE_imm8()						/* both LOOPE and LOOPZ     */
 {
     byte disp = cpu_pfq_getbyte();
 	CX--;
-    if ( ( CX != 0 ) && ZF )
+    if ( CX && ZF ) {
 		cpu_jump( CS, IP + (sigbyte)disp );
-    return;
+	}
 }
 
 void
@@ -30,55 +31,72 @@ _LOOPNE_imm8()						/* both LOOPNE and LOOPNZ   */
 {
     byte disp = cpu_pfq_getbyte();
 	CX--;
-    if ( ( CX != 0 ) && !ZF )
+    if ( CX && !ZF ) {
 		cpu_jump( CS, IP + (sigbyte)disp );
+	}
 }
 
 void
 _REP()
 {
 	byte rop = cpu_pfq_getbyte();
-	word *segpfx = CurrentSegment;
+	word *old_segment = CurrentSegment;
 
-	if((rop==0x26)||(rop==0x2E)||(rop==0x36)||(rop==0x3E)) {
-		cpu_optable[rop]();
-		segpfx = CurrentSegment;
+	/* If the following opcode is a segment override instruction,
+	 * we override our "CurrentSegment" accordingly and use the
+	 * next instruction for REPing. */
+
+	if ( rop == 0x26 || rop == 0x2E || rop == 0x36 || rop == 0x3E ) {
+		switch( rop ) {
+			case 0x26: CurrentSegment = &ES; break;
+			case 0x2E: CurrentSegment = &CS; break;
+			case 0x36: CurrentSegment = &SS; break;
+			case 0x3E: CurrentSegment = &DS; break;
+		}
 		rop = cpu_pfq_getbyte();
 	}
 
 	/* If not CMPS or SCAS -- REP, else REPE */
-	if ((rop!=0xA6)&&(rop!=0xA7)&&(rop!=0xAE)&&(rop!=0xAF)) {
-		while(CX != 0) {	/* REP	*/
-			CurrentSegment = segpfx;
+	if ( rop != 0xA6 && rop != 0xA7 && rop != 0xAE && rop != 0xAF ) {
+		while ( CX ) {	/* REP	*/
 			cpu_optable[rop]();
 			--CX;
 		}
 	} else {
-		while ( CX!=0 ) {	/* REPE/REPZ	*/
-			CurrentSegment = segpfx;
+		/* REPE / REPZ */
+		while ( CX ) {
 			cpu_optable[rop]();
 			--CX;
 			if ( !ZF )
 				break;
 		}
 	}
+	CurrentSegment = old_segment;
 }
 
 void
 _REPNE()
 {
 	byte rop = cpu_pfq_getbyte();
-	word *segpfx = CurrentSegment;
-	if((rop==0x26)||(rop==0x2E)||(rop==0x36)||(rop==0x3E)) {
-		cpu_optable[rop]();
-		segpfx = CurrentSegment;
+	word *old_segment = CurrentSegment;
+
+	/* Same as above. */
+	if ( rop == 0x26 || rop == 0x2E || rop == 0x36 || rop == 0x3E ) {
+		switch( rop ) {
+			case 0x26: CurrentSegment = &ES; break;
+			case 0x2E: CurrentSegment = &CS; break;
+			case 0x36: CurrentSegment = &SS; break;
+			case 0x3E: CurrentSegment = &DS; break;
+		}
 		rop = cpu_pfq_getbyte();
 	}
-	while ( CX != 0 ) {
-		CurrentSegment = segpfx;
+
+	while ( CX ) {
 		cpu_optable[rop]();
 		--CX;
 		if ( ZF )
 			break;
 	}
+
+	CurrentSegment = old_segment;
 }
