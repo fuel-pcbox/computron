2024d1d1b1fd7eb59769f5ac44f86dc5d4faaf9a
Andreas Kling <kling@webkit.org>
2005-09-14 15:58:12 +0000

    Cleanups + inlining.

--- 8086/flags.c
+++ 8086/flags.c
@@ -8,44 +8,54 @@
 
 #include "vomit.h"
 
-byte parity_table[256];
+static byte cpu_parity( byte );
+static byte parity_table[256];
 
-void cpu_flags_init() {
-	register int i;
-	for(i=0;i<256;i++)
-		parity_table[i]=cpu_parity(i);
-
-	CF=0; DF=0; TF=0; PF=0;
-	AF=0; ZF=0; SF=0; IF=0; OF=0;
-    
-	return;
-}
-
-void cpu_setZF(dword ans) {
-	if(ans==0) ZF=1; else ZF=0;
-}
-
-void cpu_setSF(dword ans, byte bits) {
-	if(bits==16)
-		SF=(ans>>15)&1;
-	else
-		SF=(ans>>7)&1;
-}
-
-void cpu_setPF(dword ans) {
-    PF=parity_table[ans&0xFF];
-}
-
-void cpu_setAF(dword ans, word src, word dest) {
-	AF=((((ans)^((src)^(dest)))&0x10)>>4)&1;
-}
-
-byte cpu_parity(byte b) {
-	byte y, z=0, x=1;
-	for(y=0;y<8;y++) {
-		if(b&x) z++;
-		x=x<<1;
+void
+cpu_flags_init()
+{
+	int i;
+	for ( i = 0; i < 256; ++i ) {
+		parity_table[i] = cpu_parity( i );
 	}
-	return !(z&1);
+
+	CF = DF = TF = PF = AF = ZF = SF = IF = OF = 0;
+}
+
+inline void
+cpu_setZF( dword ans )
+{
+	ZF = ( ans == 0 );
+}
+
+inline void
+cpu_setSF( dword ans, byte bits )
+{
+	if ( bits == 16 )
+		SF = ( ans >> 15 ) & 1;
+	else
+		SF = ( ans >> 7 ) & 1;
+}
+
+inline void
+cpu_setPF( dword ans )
+{
+    PF = parity_table[ans & 0xFF];
+}
+
+inline void
+cpu_setAF( dword ans, word src, word dest )
+{
+	AF = ( ( ( ans ^ ( src ^ dest ) ) & 0x10 ) >> 4) & 1;
+}
+
+static byte cpu_parity(byte b) {
+	byte y, z = 0, x = 1;
+	for ( y = 0; y < 8; ++y ) {
+		if ( b & x )
+			z++;
+		x = x << 1;
+	}
+	return !( z & 1 );
 }
 
