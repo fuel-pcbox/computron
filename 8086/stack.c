cbd3fc12f9595eb2c50cfcb91aa67d5bc9704d60
Andreas Kling <kling@webkit.org>
2005-10-01 10:43:41 +0000

    Formatting.

--- 8086/stack.c
+++ 8086/stack.c
@@ -6,38 +6,91 @@
 #include "vomit.h"
 #include <stdio.h>
 
-void _PUSH_reg16() { mem_push(*treg16[cpu_opcode&7]); }
-void _POP_reg16() { *treg16[cpu_opcode&7]=mem_pop(); }
 void
-_PUSH_RM16() {
-	byte rm = cpu_rmbyte;
-	word *p = cpu_rmptr(rm, 16);
-	mem_push(*p);
+_PUSH_reg16()
+{
+	mem_push( *treg16[cpu_opcode & 7] );
 }
+
 void
-_POP_RM16() {
-	byte rm = cpu_rmbyte;
-	word *p = cpu_rmptr(rm, 16);
+_POP_reg16()
+{
+	*treg16[cpu_opcode & 7] = mem_pop();
+}
+
+void
+_PUSH_RM16()
+{
+	word *p = cpu_rmptr( cpu_rmbyte, 16 );
+	mem_push( *p );
+}
+
+void
+_POP_RM16()
+{
+	word *p = cpu_rmptr( cpu_rmbyte, 16 );
 	*p = mem_pop();
 }
 
-void _PUSH_CS() { mem_push(CS); }
-void _PUSH_DS() { mem_push(DS); }
-void _PUSH_ES() { mem_push(ES); }
-void _PUSH_SS() { mem_push(SS); }
+void
+_PUSH_CS()
+{
+	mem_push( CS );
+}
 
 void
-_POP_CS() {
-	#ifdef VM_DEBUG
-		printf("%04X:%04X ", BCS, BIP);
-	#endif
-	printf("\npanic: Attempted either POP CS or 286+ instruction.\n");
-	vm_exit(1);
+_PUSH_DS()
+{
+	mem_push( DS );
 }
-void _POP_DS() { DS = mem_pop(); }
-void _POP_ES() { ES = mem_pop(); }
-void _POP_SS() { SS = mem_pop(); }
 
-void _PUSHF() { mem_push(cpu_getflags()); }
-void _POPF() { cpu_setflags(mem_pop()); }
+void
+_PUSH_ES()
+{
+	mem_push( ES );
+}
 
+void
+_PUSH_SS()
+{
+	mem_push( SS );
+}
+
+void
+_POP_CS()
+{
+#ifdef VM_DEBUG
+	printf( "%04X:%04X ", BCS, BIP );
+#endif
+	printf( "\npanic: Attempted either POP CS or 286+ instruction.\n" );
+	vm_exit( 1 );
+}
+void
+_POP_DS()
+{
+	DS = mem_pop();
+}
+
+void
+_POP_ES()
+{
+	ES = mem_pop();
+}
+
+void
+_POP_SS()
+{
+	SS = mem_pop();
+}
+
+void
+_PUSHF()
+{
+	mem_push( cpu_getflags() );
+}
+
+void
+_POPF()
+{
+	cpu_setflags( mem_pop() );
+}
