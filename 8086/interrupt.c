7b27ea9345fcbae589063e84a9a8124c5cefd83c
Andreas Kling <kling@webkit.org>
2006-03-19 21:45:06 +0000

    Cleanups and comments.

--- 8086/interrupt.c
+++ 8086/interrupt.c
@@ -4,23 +4,30 @@
  */
 
 #include "vomit.h"
-#include <stdio.h>
 
 void
-_INT_imm8() {
+_INT_imm8()
+{
 	byte imm = cpu_pfq_getbyte();
-	int_call(imm);
+	int_call( imm );
 }
 
 void
-_INT3() {
-	int_call(0x03);
+_INT3()
+{
+	int_call( 3 );
 }
 
 void
-_INTO() {
-	if(OF==1)
-		int_call(0x04);
+_INTO()
+{
+	/* XXX: I've never seen this used, so it's probably good to log it. */
+	vlog( VM_ALERT, "INTO used, can you believe it?" );
+
+	if( OF == 1 )
+	{
+		int_call( 4 );
+	}
 }
 
 void
@@ -33,28 +40,37 @@ _IRET()
 }
 
 void
-int_call( byte isr ) {
-	#ifdef VM_DEBUG
-		char tmp[40];
-		if(trapint) {
-			sprintf(tmp, "%04X:%04X cpu: Interrupt %02X,%02X trapped.\n", BCS, BIP, isr, *treg8[REG_AH]);
-			vm_out(tmp, VM_LOGMSG);
-		}
-	#endif
+int_call( byte isr )
+{
+#ifdef VM_DEBUG
+	if( trapint )
+	{
+		vlog( VM_CPUMSG, "%04X:%04X Interrupt %02X,%02X trapped", BCS, BIP, isr, *treg8[REG_AH] );
+	}
+#endif
+
+	/* XXX: VGA BIOS is currently residing in C land for... convenience.
+	 *      I realize this breaks anything that relies on hooking. */
 	if( isr == 0x10 )
 	{
 		bios_interrupt10();
 		return;
 	}
-	mem_push(cpu_getflags());
+
+	mem_push( cpu_getflags() );
 	IF = 0;
 	TF = 0;
-	mem_push(CS);
-	mem_push(IP);
+	mem_push( CS );
+	mem_push( IP );
+
+	/* TODO: Get rid of this ugly mess. */
 #ifdef VM_DEBUG
-	if(!mempeek) {
+	if( !mempeek )
+	{
 		cpu_jump(mem_getword(0, isr*4+2), mem_getword(0, isr*4));
-	} else {
+	}
+	else
+	{
 		mempeek = 0;
 #endif
 		cpu_jump(mem_getword(0, isr*4+2), mem_getword(0, isr*4));
