bcd9da99bcc24c5b8532f3c0b4717d2802e2e5a7
Andreas Kling <kling@webkit.org>
2006-04-07 19:59:02 +0000

    Much cleaner, slightly recursive REP/REPZ implementation.

--- 8086/loop.c
+++ 8086/loop.c
@@ -38,68 +38,51 @@ _LOOPNE_imm8()						/* both LOOPNE and LOOPNZ   */
 	}
 }
 
+#define DO_REP( func ) for( ; cpu.regs.W.CX; --cpu.regs.W.CX ) { func (); }
+#define DO_REPZ( func ) for( cpu.ZF = should_equal; cpu.regs.W.CX && (cpu.ZF == should_equal ); --cpu.regs.W.CX ) { func (); }
+
+static void
+__rep( byte opcode, bool should_equal )
+{
+	switch( opcode )
+	{
+		case 0x26: cpu.CurrentSegment = &cpu.ES; break;
+		case 0x2E: cpu.CurrentSegment = &cpu.CS; break;
+		case 0x36: cpu.CurrentSegment = &cpu.SS; break;
+		case 0x3E: cpu.CurrentSegment = &cpu.DS; break;
+
+		case 0xA4: DO_REP( _MOVSB ); return;
+		case 0xA5: DO_REP( _MOVSW ); return;
+		case 0xAA: DO_REP( _STOSB ); return;
+		case 0xAB: DO_REP( _STOSW ); return;
+		case 0xAC: DO_REP( _LODSB ); return;
+		case 0xAD: DO_REP( _LODSW ); return;
+
+		case 0xA6: DO_REPZ( _CMPSB ); return;
+		case 0xA7: DO_REPZ( _CMPSW ); return;
+		case 0xAE: DO_REPZ( _SCASB ); return;
+		case 0xAF: DO_REPZ( _SCASW ); return;
+
+		default: cpu_optable[opcode](); return;
+	}
+
+	/* Recurse if this opcode was a segment prefix. */
+	/* FIXME: Infinite recursion IS possible here. */
+	__rep( cpu_pfq_getbyte(), should_equal );
+}
+
 void
 _REP()
 {
-	byte rop = cpu_pfq_getbyte();
 	word *old_segment = cpu.CurrentSegment;
-
-	/* If the following opcode is a segment override instruction,
-	 * we override our "CurrentSegment" accordingly and use the
-	 * next instruction for REPing. */
-
-	if ( rop == 0x26 || rop == 0x2E || rop == 0x36 || rop == 0x3E ) {
-		switch( rop ) {
-			case 0x26: cpu.CurrentSegment = &cpu.ES; break;
-			case 0x2E: cpu.CurrentSegment = &cpu.CS; break;
-			case 0x36: cpu.CurrentSegment = &cpu.SS; break;
-			case 0x3E: cpu.CurrentSegment = &cpu.DS; break;
-		}
-		rop = cpu_pfq_getbyte();
-	}
-
-	/* If not CMPS or SCAS -- REP, else REPE */
-	if ( rop != 0xA6 && rop != 0xA7 && rop != 0xAE && rop != 0xAF ) {
-		while( cpu.regs.W.CX ) {	/* REP	*/
-			cpu_optable[rop]();
-			--cpu.regs.W.CX;
-		}
-	} else {
-		/* REPE / REPZ */
-		while( cpu.regs.W.CX ) {
-			cpu_optable[rop]();
-			--cpu.regs.W.CX;
-			if ( !cpu.ZF )
-				break;
-		}
-	}
+	__rep( cpu_pfq_getbyte(), true );
 	cpu.CurrentSegment = old_segment;
 }
 
 void
 _REPNE()
 {
-	byte rop = cpu_pfq_getbyte();
 	word *old_segment = cpu.CurrentSegment;
-
-	/* Same as above. */
-	if ( rop == 0x26 || rop == 0x2E || rop == 0x36 || rop == 0x3E ) {
-		switch( rop ) {
-			case 0x26: cpu.CurrentSegment = &cpu.ES; break;
-			case 0x2E: cpu.CurrentSegment = &cpu.CS; break;
-			case 0x36: cpu.CurrentSegment = &cpu.SS; break;
-			case 0x3E: cpu.CurrentSegment = &cpu.DS; break;
-		}
-		rop = cpu_pfq_getbyte();
-	}
-
-	while( cpu.regs.W.CX )
-	{
-		cpu_optable[rop]();
-		--cpu.regs.W.CX;
-		if ( cpu.ZF )
-			break;
-	}
-
+	__rep( cpu_pfq_getbyte(), false );
 	cpu.CurrentSegment = old_segment;
 }
