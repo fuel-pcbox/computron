afaddd3822b4c4a52c27c7fe363faa86186aab85
Andreas Kling <kling@webkit.org>
2010-05-25 00:37:51 +0200

    Remove dummy I/O handlers from hw/gameport (they had nothing to do with gameport)

--- hw/gameport.cpp
+++ hw/gameport.cpp
@@ -1,7 +1,7 @@
 /*
- * Gameport...
+ * Gameport
  *
- * Not really an emulation, just setting up some dummy I/O handlers
+ * Not really an emulation, just setting up a dummy I/O handlers
  * so it won't spam me about reading port 0x201..
  */
 
@@ -13,11 +13,6 @@ static void gameport_write(VCpu* cpu, WORD port, BYTE data);
 void gameport_init()
 {
 	vm_listen(0x201, gameport_read, gameport_write);
-
-	// FIXME: These 2 are not at all gameport related
-	// I'm just ignoring them for now...
-	vm_listen(0x388, gameport_read, gameport_write);
-	vm_listen(0x389, gameport_read, gameport_write);
 }
 
 BYTE gameport_read(VCpu*, WORD)
