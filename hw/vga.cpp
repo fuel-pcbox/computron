57e246bdc08ec20853e43236633e4005a22a18b0
Andreas Kling <kling@webkit.org>
2010-05-26 03:27:17 +0200

    Use separate indices for reading and writing DAC registers.

--- hw/vga.cpp
+++ hw/vga.cpp
@@ -26,8 +26,10 @@ static byte index_palette = 0;
 static byte columns;
 static byte rows;
 
-static byte dac_data_index = 0;
-static byte dac_data_subindex = 0;
+static byte dac_data_read_index = 0;
+static byte dac_data_read_subindex = 0;
+static byte dac_data_write_index = 0;
+static byte dac_data_write_subindex = 0;
 
 static QMutex s_paletteMutex;
 
@@ -45,6 +47,7 @@ static byte vga_get_current_register(VCpu*, word );
 static void vga_write_3c0(VCpu*, word port, byte data );
 static byte vga_read_3c1(VCpu*, word port );
 static byte vga_read_miscellaneous_output_register(VCpu*, word port );
+static void vga_dac_read_address(VCpu*, WORD, BYTE);
 static void vga_dac_write_address(VCpu*, WORD, BYTE);
 static BYTE vga_dac_read_data(VCpu*, WORD);
 static void vga_dac_write_data(VCpu*, WORD, BYTE);
@@ -84,6 +87,7 @@ vga_init()
 
     vm_listen( 0x3c4, 0L, vga_selseq );
     vm_listen( 0x3c5, vga_getseq, vga_setseq );
+    vm_listen(0x3c7, 0L, vga_dac_read_address);
     vm_listen(0x3c8, 0L, vga_dac_write_address);
     vm_listen(0x3c9, vga_dac_read_data, vga_dac_write_data);
     vm_listen(0x3ca, vga_fcr_read, 0L);
@@ -104,8 +108,10 @@ vga_init()
 
     io_sequencer[2] = 0x0F;
 
-    dac_data_index = 0;
-    dac_data_subindex = 0;
+    dac_data_read_index = 0;
+    dac_data_read_subindex = 0;
+    dac_data_write_index = 0;
+    dac_data_write_subindex = 0;
 }
 
 void
@@ -314,31 +320,36 @@ bool is_palette_dirty()
 
 void vga_dac_write_address(VCpu*, WORD, BYTE data)
 {
-    // vlog(VM_VIDEOMSG, "DAC register %02X selected", data);
-    dac_data_index = data;
-    dac_data_subindex = 0;
+    dac_data_write_index = data;
+    dac_data_write_subindex = 0;
+}
+
+void vga_dac_read_address(VCpu*, WORD, BYTE data)
+{
+    dac_data_read_index = data;
+    dac_data_read_subindex = 0;
 }
 
 BYTE vga_dac_read_data(VCpu*, WORD)
 {
     BYTE data = 0;
-    switch (dac_data_subindex) {
+    switch (dac_data_read_subindex) {
     case 0:
-        data = vga_color_register[dac_data_index].r;
+        data = vga_color_register[dac_data_read_index].r;
         break;
     case 1:
-        data = vga_color_register[dac_data_index].g;
+        data = vga_color_register[dac_data_read_index].g;
         break;
     case 2:
-        data = vga_color_register[dac_data_index].b;
+        data = vga_color_register[dac_data_read_index].b;
         break;
     }
 
-    // vlog(VM_VIDEOMSG, "Reading component %u of color %02X (%02X)", dac_data_subindex, dac_data_index, data);
+    // vlog(VM_VIDEOMSG, "Reading component %u of color %02X (%02X)", dac_data_read_subindex, dac_data_read_index, data);
 
-    if (++dac_data_subindex >= 3) {
-        dac_data_subindex = 0;
-        ++dac_data_index;
+    if (++dac_data_read_subindex >= 3) {
+        dac_data_read_subindex = 0;
+        ++dac_data_read_index;
     }
 
     return data;
@@ -347,21 +358,21 @@ BYTE vga_dac_read_data(VCpu*, WORD)
 void vga_dac_write_data(VCpu* cpu, WORD, BYTE data)
 {
     // vlog(VM_VIDEOMSG, "Setting component %u of color %02X to %02X", dac_data_subindex, dac_data_index, data);
-    switch (dac_data_subindex) {
+    switch (dac_data_write_subindex) {
     case 0:
-        vga_color_register[dac_data_index].r = data;
+        vga_color_register[dac_data_write_index].r = data;
         break;
     case 1:
-        vga_color_register[dac_data_index].g = data;
+        vga_color_register[dac_data_write_index].g = data;
         break;
     case 2:
-        vga_color_register[dac_data_index].b = data;
+        vga_color_register[dac_data_write_index].b = data;
         break;
     }
 
-    if (++dac_data_subindex >= 3) {
-        dac_data_subindex = 0;
-        ++dac_data_index;
+    if (++dac_data_write_subindex >= 3) {
+        dac_data_write_subindex = 0;
+        ++dac_data_write_index;
     }
 
     mark_palette_dirty();
