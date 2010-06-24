1893cbe8b504a215fb402dd4e728dea45de1900e
Andreas Kling <kling@webkit.org>
2010-06-24 06:27:19 +0200

    Add QWORD and SIGNED_QWORD types.

--- include/types.h
+++ include/types.h
@@ -13,8 +13,10 @@ typedef int32_t sigdword;
 typedef uint8_t BYTE;
 typedef uint16_t WORD;
 typedef uint32_t DWORD;
+typedef uint64_t QWORD;
 typedef int8_t SIGNED_BYTE;
 typedef int16_t SIGNED_WORD;
 typedef int32_t SIGNED_DWORD;
+typedef int64_t SIGNED_QWORD;
 
 #endif
