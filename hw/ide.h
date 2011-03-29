f9d84ad497ab9a91d500498c427e5fb4dfcd686a
Andreas Kling <kling@webkit.org>
2011-03-29 20:55:29 +0200

    Add missing header (ide.h)

---
+++ hw/ide.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2003-2011 Andreas Kling <kling@webkit.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANDREAS KLING ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL ANDREAS KLING OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef IDE_H
+#define IDE_H
+
+#include "iodevice.h"
+
+class IDE : public IODevice
+{
+public:
+    enum Status {
+        ERROR = 0x01,
+        INDEX = 0x02,
+        CORR  = 0x04,
+        DRQ   = 0x08,
+        DSC   = 0x10,
+        DWF   = 0x20,
+        DRDY  = 0x40,
+        BUSY  = 0x80
+    };
+
+    IDE();
+    virtual ~IDE();
+
+    virtual BYTE in8(WORD port);
+    virtual void out8(WORD port, BYTE data);
+
+    Status status() const;
+
+    static IDE* the();
+
+private:
+    struct Private;
+    Private* d;
+};
+
+#endif
