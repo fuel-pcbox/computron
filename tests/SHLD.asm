6b1dbe03794ed2ee6220ad4679b68d24f8730042
Andreas Kling <awesomekling@gmail.com>
2018-02-15 23:50:19 +0100

    Add test coverage for SHLD/SHRD with CL as step count.

--- tests/SHLD.asm
+++ tests/SHLD.asm
@@ -4,4 +4,9 @@ mov eax, 0x01234567
 mov ebx, 0x89ABCDEF
 shld eax, ebx, 4
 
+mov esi, 0x01234567
+mov edi, 0x89ABCDEF
+mov cl, 4
+shld esi, edi, cl
+
 db 0xf1
--- tests/SHRD.asm
+++ tests/SHRD.asm
@@ -4,4 +4,9 @@ mov eax, 0x01234567
 mov ebx, 0x89ABCDEF
 shrd eax, ebx, 4
 
+mov esi, 0x01234567
+mov edi, 0x89ABCDEF
+mov cl, 4
+shrd esi, edi, cl
+
 db 0xf1
